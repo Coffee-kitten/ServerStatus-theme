9464142af806f013654af9dffd98906ca807a99c
Coffee-kitten <klcherish@gmail.com>
2024-04-10 22:38:16 +0800

    update Collapse 折叠面板

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,19 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
-import pluginForms from '@tailwindcss/forms'
+import defaultTheme from "tailwindcss/defaultTheme";
+import pluginForms from "@tailwindcss/forms";
 
 /** @type { import('tailwindcss').Config } */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Noto Sans SC', ...defaultTheme.fontFamily.sans],
-        titillium: ['Titillium Web', ...defaultTheme.fontFamily.sans],
+        sans: ["Noto Sans SC", ...defaultTheme.fontFamily.sans],
+        titillium: ["Titillium Web", ...defaultTheme.fontFamily.sans],
       },
       screens: {
-        sm: '500px'
-      }
+        sm: "500px",
+      },
     },
   },
-  plugins: [
-    pluginForms
-  ],
-}
+  plugins: [require("daisyui")],
+};
